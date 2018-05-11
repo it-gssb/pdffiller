44e214712d0a7f650cc1482f871224661f3e2067
michaelsassin <michaelsassin@michaels-imac.fios-router.home>
2018-05-11 13:34:09 -0400

    Skip pdf form substitution if PDF file does not contain a form.

--- src/main/java/org/gssb/pdffiller/pdf/PdfFormFiller.java
+++ src/main/java/org/gssb/pdffiller/pdf/PdfFormFiller.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
 import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
@@ -17,7 +19,10 @@ import org.gssb.pdffiller.excel.ExcelCell;
 import org.gssb.pdffiller.excel.ExcelRow;
 
 public class PdfFormFiller {
-//   private final static Logger logger = LogManager.getLogger(PdfFormFiller.class);
+   private final static Logger logger = LogManager.getLogger(PdfFormFiller.class);
+   
+   private static final String NON_FORM_WARN = 
+         "The file '%s' does not contain a PDF form and will be copied as is.";
    
    private static final int    KEY_STRENGTH = 128;
 
@@ -57,28 +62,33 @@ public class PdfFormFiller {
       PDDocumentCatalog docCatalog = pdf.getDocumentCatalog();
       PDAcroForm acroForm = docCatalog.getAcroForm();
       
-      Set<String> fieldNames =
-            acroForm.getFields()
-                    .stream()
-                    .map(f -> f.getFullyQualifiedName())
-                    .collect(Collectors.toSet());
-      acroForm.setNeedAppearances(false);
-      
-      // iterate over all pdf fields
-      for (String acroFieldName : fieldNames) {
-         String columnName = fieldMap.getOrDefault(acroFieldName, acroFieldName);
-         ExcelCell cell = excelRow.getRow().get(columnName);
+      if (acroForm!=null) {
+         Set<String> fieldNames =
+               acroForm.getFields()
+                       .stream()
+                       .map(f -> f.getFullyQualifiedName())
+                       .collect(Collectors.toSet());
+         acroForm.setNeedAppearances(false);
          
-         if (cell==null) continue;
+         // iterate over all pdf fields
+         for (String acroFieldName : fieldNames) {
+            String columnName = fieldMap.getOrDefault(acroFieldName, acroFieldName);
+            ExcelCell cell = excelRow.getRow().get(columnName);
+            
+            if (cell==null) continue;
+            
+            setField(acroForm, acroFieldName, cell.getColumnValue());
+         }
          
-         setField(acroForm, acroFieldName, cell.getColumnValue());
-      }
+         acroForm.flatten();
       
-      acroForm.flatten();
-      
-      if (masterKey!=null && !masterKey.isEmpty() && 
-          secret!=null && !secret.isEmpty() && !secret.equals("0")) {
-        encrypt(pdf, masterKey, secret);
+         if (masterKey!=null && !masterKey.isEmpty() && 
+             secret!=null && !secret.isEmpty() && !secret.equals("0")) {
+           encrypt(pdf, masterKey, secret);
+         }
+      } else {
+         String msg = String.format(NON_FORM_WARN, templatePdf.getName());
+         logger.warn(msg);
       }
       pdf.save(targetPdf);
       pdf.close();
